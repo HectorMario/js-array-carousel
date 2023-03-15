1f2eea09b41dcaf5e89da18e1d8de689a5ad9282
HectorMario <hector.mario.peraz@gmail.com>
2023-03-15 11:14:14 +0100

    finitto

--- js/script.js
+++ js/script.js
@@ -8,32 +8,35 @@ const pictur = [
 
 const giu = document.getElementById("down");
 const su = document.getElementById("up");
+const yu = document.getElementsByClassName("border")
 let i = 0;
 
 
 giu.addEventListener("click", function(){
+    yu[i].classList.remove("ms-border")
     i += 1;
-    document.getElementById("xd").src= pictur[i]
     document.getElementById("se").src= pictur[i]
     console.log(i);
     if (i > pictur.length - 1){
-        i= 0
-        document.getElementById("xd").src= pictur[i]
+        i= 0;
         document.getElementById("se").src= pictur[i]
+        yu[i].classList.add("ms-border");
     }
+    yu[i].classList.add("ms-border");
 })
 
 
 su.addEventListener("click", function(){
+    yu[i].classList.remove("ms-border")
     i -= 1;
-    document.getElementById("xd").src= pictur[i]
     document.getElementById("se").src= pictur[i]
     console.log(i);
     if (i < 0){
         i  = pictur.length - 1;
-        document.getElementById("xd").src= pictur[i]
         document.getElementById("se").src= pictur[i]
+        yu[i].classList.add("ms-border");     
     }
+    yu[i].classList.add("ms-border");
 })
 
 
